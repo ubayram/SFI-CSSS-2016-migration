966b29a4fbd3c4b490748d4a2c5018b53cbbb0b2
Christopher Revell <cr395@cam.ac.uk>
2016-06-28 21:42:32 -0600

    Script to convert to scalar time data

--- data_filter/old_code/individual_separator.py
+++ data_filter/old_code/individual_separator.py
@@ -15,7 +15,7 @@ bird_ID_list = []
 #If the ID is not yet in the bird_ID_list list, add it to the list.
 for line in infile:
     line_stripped = line.rstrip()
-    line_list = line_stripped.split("  ")
+    line_list = line_stripped.split(" ")
     bird_ID = line_list[-1]
     if bird_ID not in bird_ID_list:
         bird_ID_list.append(bird_ID)
@@ -23,7 +23,7 @@ infile.close()
 #bird_ID_list now contains the ID labels of all individual birds in this data set
 
 #For each bird ID in the bird_ID_list list, open a new file labelled with the bird ID
-#Write all lines from the original file corresponding to this ID to the new file. 
+#Write all lines from the original file corresponding to this ID to the new file.
 for bird in bird_ID_list:
     outfile = open(infilename[0:-4]+"_"+bird[1:-1]+".txt",'w')
     infile = open(infilename,'r')
